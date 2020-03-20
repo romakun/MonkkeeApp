75d1093560867ce1e9ad46147a3e79f17ff6d9e7
RomaKun <roman@gophotoweb.ru>
2020-03-20 20:42:15 +0300

    Merge branch 'master' of C:\MonkkeeApp with conflicts.

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,8 +1,23 @@
 package pages;
 
-public abstract class BasePage {
-    abstract BasePage openPage();
-    abstract void isPageOpened();
-    By.
+import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.SelenideElement;
+import lombok.extern.log4j.Log4j2;
 
+import java.util.NoSuchElementException;
+
+
+@Log4j2
+public abstract class BasePage {
+
+    abstract BasePage openPage();
+
+    abstract void isPageOpened();
+
+    public SelenideElement element(SelenideElement element){
+        String script = "arguments[0].style.border='3px solid red'";
+        Selenide.executeJavaScript(script, $(element));
+        return element;
+    }
 }
---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
+
+public class MainPage extends BasePage {
+
+    private static final By CREATE_ENTRY_BUTTON_ID = id("create-entry");
+    private static final By DELETE_ENTRY_BUTTON_ID = id("delete-entries");
+    private static final String SELECT_ALL_CHECKBOX_CSS = "[title='Select all']";
+    private static final String ENTRY_CHECKBOX_LOCATOR_CSS = "[ng-change='updateSelectionState()']";
+    private static final String ENTRY_LOCATOR_CSS = ".entry";
+    private static final String MANAGE_TAGS_LINK_TEXT = "Manage tags";
+    private static final By SEARCH_INPUT_ID = id("appendedInputButton");
+    private static final String SEARCH_BUTTON_CSS = "[title='Search']";
+
+    //CALENDAR LOCATORS
+    private static final By CALENDAR_INPUT_ID = id("datepicker");
+    private static final String CALENDAR_DAYS_PREV_CSS = ".datepicker-days .prev";
+    private static final String CALENDAR_DAYS_NEXT_CSS = ".datepicker-days .next";
+    private static final String CALENDAR_SWITCH_TO_MONTHS_CSS = ".datepicker-days .switch";
+    private static final String CALENDAR_MONTHS_PREV_CSS = ".datepicker-months .prev";
+    private static final String CALENDAR_MONTHS_NEXT_CSS = ".datepicker-months .next";
+    private static final String CALENDAR_SWITCH_TO_YEARS_CSS = ".datepicker-months .switch";
+    private static final String CALENDAR_YEARS_PREV_CSS = ".datepicker-years .prev";
+    private static final String CALENDAR_YEARS_NEXT_CSS = ".datepicker-years .next";
+    private static final By CALENDAR_ACTIVE_DAY_LOCATOR = xpath("//td[contains(@class='day') and contains(text(),'18')]");
+    private static final By CALENDAR_MONTH_LOCATOR = xpath("//span[contains(@class,'month') and contains(text(),'2021')]");
+    private static final By CALENDAR_YEAR_LOCATOR = xpath("//span[contains(@class,'year') and contains(text(),'2021')]");
+
+    @Override
+    public MainPage openPage() {
+        isPageOpened();
+        return this;
+    }
+
+    @Override
+    void isPageOpened() {
+        $(CREATE_ENTRY_BUTTON_ID).shouldBe(Condition.visible);
+    }
+
+    
+}
