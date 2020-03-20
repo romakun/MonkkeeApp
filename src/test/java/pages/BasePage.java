e6fc35a3bc929eea8c2b216c230611088d6a8772
RomaKun <roman@gophotoweb.ru>
2020-03-20 21:19:42 +0300

    Обернул еще в try catch, ничего лучше не придумал с возвращаемым значением

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,10 +2,10 @@ package pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ex.ElementNotFound;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
 
 @Log4j2
 public abstract class BasePage {
@@ -16,27 +16,47 @@ public abstract class BasePage {
 
     public SelenideElement $(By element, String logMessage){
         String script = "arguments[0].style.border='3px solid red'";
-        Selenide.executeJavaScript(script, Selenide.$(element));
-        log.info(logMessage);
-        return Selenide.$(element);
+        try {
+            Selenide.executeJavaScript(script, Selenide.$(element));
+            log.info(logMessage);
+            return Selenide.$(element);
+        } catch (ElementNotFound e) {
+             log.error("Не найден элемент " + element);
+             return null;
+        }
     }
 
     public SelenideElement $(By element){
         String script = "arguments[0].style.border='3px solid red'";
-        Selenide.executeJavaScript(script, Selenide.$(element));
-        return Selenide.$(element);
+        try {
+            Selenide.executeJavaScript(script, Selenide.$(element));
+            return Selenide.$(element);
+        } catch (ElementNotFound e) {
+            log.error("Не найден элемент " + element);
+            return null;
+        }
     }
 
     public SelenideElement $(String element){
         String script = "arguments[0].style.border='3px solid red'";
-        Selenide.executeJavaScript(script, Selenide.$(element));
-        return Selenide.$(element);
+        try {
+            Selenide.executeJavaScript(script, Selenide.$(element));
+            return Selenide.$(element);
+        } catch (ElementNotFound e) {
+            log.error("Не найден элемент " + element);
+            return null;
+        }
     }
 
     public SelenideElement $(String element, String logMessage){
         String script = "arguments[0].style.border='3px solid red'";
-        Selenide.executeJavaScript(script, Selenide.$(element));
-        log.info(logMessage);
-        return Selenide.$(element);
+        try {
+            Selenide.executeJavaScript(script, Selenide.$(element));
+            log.info(logMessage);
+            return Selenide.$(element);
+        } catch (ElementNotFound e) {
+            log.error("Не найден элемент " + element);
+            return null;
+        }
     }
 }
